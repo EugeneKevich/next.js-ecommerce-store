a426ab52352ecb79b0b5631a58bb859683be12b7
Eugene Stashkevych <eugene.kevich@gmail.com>
2023-01-03 21:05:43 +0100

    Update Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,43 @@
 import { css } from '@emotion/react';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import example1 from '../public/home1.jpg';
 import example2 from '../public/home2.jpg';
 import kayak1 from '../public/kayakWall2.jpg';
 
-const bannerStyles = css`
-  height: 800px;
+const mainStyle = css`
+  width: 100%;
 `;
-const sectionStyles = css``;
+
+const sectionStyles = css`
+  text-align: center;
+`;
 
 const productsStyles = css`
+  margin: auto;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  width: 1200px;
+
+  img {
+    border-radius: 15px;
+    margin-bottom: 30px;
+  }
 `;
 
-const buttonStyles = css`
-  color: #015a8d;
-  border: 1px solid;
-  border-radius: 50%;
-  width: 350px;
-  height: 350px;
-  text-align: center;
+const buttonStyle = css`
+  margin: 30px 0;
+  background-color: #015a8d;
+  color: #fff;
+  border-radius: 15px;
+  width: 180px;
+  height: 45px;
+  border: none;
+  a {
+    font-size: 20px;
+    color: #fff;
+  }
 `;
 
 export default function Home() {
@@ -31,22 +47,22 @@ export default function Home() {
         <title> Home</title>
         <meta name="description" content="Kayaks store" />
       </Head>
-      <div css={bannerStyles}>
-        {' '}
-        <Image src={kayak1} alt="weddwd" />
-      </div>
-      <section css={sectionStyles}>
-        <div>
+      <div css={mainStyle}>
+        <Image src={kayak1} alt="weddwd" height="700px" />
+
+        <section css={sectionStyles}>
           <h1>Products</h1>
-        </div>
-        <div css={productsStyles}>
-          <Image src={example1} alt="sdq" />
-          <Image src={example2} alt="wqs" />
-        </div>
-        <div href="" css={buttonStyles}>
-          <a>More Kayaks</a>
-        </div>
-      </section>
+
+          <div css={productsStyles}>
+            <Image src={example1} alt="sdq" width="500px" height="200px" />
+            <Image src={example2} alt="wqs" width="500px" height="200px" />
+          </div>
+
+          <button css={buttonStyle}>
+            <Link href="/products">More Kayaks</Link>
+          </button>
+        </section>
+      </div>
     </>
   );
 }
